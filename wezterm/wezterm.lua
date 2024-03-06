8211c6e8ceb9bc996f8bec70cb6496e0213713da
Chen Yulin <chenyulin@sjtu.edu.cn>
2024-03-06 20:08:57 +0800

    feat. better waybar

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -7,25 +7,25 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'EverforestDark (Gogh)'
+config.color_scheme = "EverforestDark (Gogh)"
 
 config.font_size = 16.0
 config.enable_tab_bar = false
-config.window_background_opacity = 0.8
+config.window_background_opacity = 0.9
 local act = wezterm.action
 
 -- customize mouse scroll speed but not working for touchpad
 config.mouse_bindings = {
-  {
-    event = { Down = { streak = 3, button = { WheelUp = 1 } } },
-    mods = 'NONE',
-    action = act.ScrollByLine(-3),
-  },
-  {
-    event = { Down = { streak = 3, button = { WheelDown = 1 } } },
-    mods = 'NONE',
-    action = act.ScrollByLine(3),
-  },
+	{
+		event = { Down = { streak = 3, button = { WheelUp = 1 } } },
+		mods = "NONE",
+		action = act.ScrollByLine(-3),
+	},
+	{
+		event = { Down = { streak = 3, button = { WheelDown = 1 } } },
+		mods = "NONE",
+		action = act.ScrollByLine(3),
+	},
 }
 
 -- and finally, return the configuration to wezterm
